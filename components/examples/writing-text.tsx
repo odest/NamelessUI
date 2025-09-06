59bbaed1dca2a77f3b5198ede7e8b5e9f0618696
dest <destrochloridium@gmail.com>
2025-09-06 21:58:03 +0300

    feat(core): add WritingText component, example, docs, registry

---
+++ components/examples/writing-text.tsx
@@ -0,0 +1,11 @@
+import { WritingText } from "@/components/core/writing-text";
+
+export default function WritingTextExample() {
+  return (
+    <WritingText
+      className="text-4xl"
+      text="Writing Text With Effect"
+      spacing={9}
+    />
+  );
+}
